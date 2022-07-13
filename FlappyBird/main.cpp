9a24118652f0581763335fb9cd3e7ecf89f0bd05
ArekBrodowski <106244256+ArekBrodowski@users.noreply.github.com>
2022-07-13 17:58:24 +0200

    Initial Commit

---
+++ FlappyBird/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FlappyBird
+//
+//  Created by Arek Brodowski on 13/07/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
